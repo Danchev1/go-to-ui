247b46a5e2caee93789c9d9bebc7881459647f03
yordandanchev <yordangdanchev@gmail.com>
2025-03-15 17:20:44 +0200

    - Add Alert Story for Alert Component

---
+++ lib/Alert/Alert.stories.tsx
@@ -0,0 +1,37 @@
+import { Alert, AlertDescription, AlertTitle } from './Alert';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Terminal } from 'lucide-react';
+
+const meta = {
+  title: 'Design System/Atoms/Alert',
+  component: Alert,
+  parameters: {
+    layout: 'centered'
+  },
+  argTypes: {
+    variant: {
+      options: ['default', 'destructive'],
+      control: { type: 'select' }
+    }
+  }
+} satisfies Meta<typeof Alert>;
+
+export default meta;
+
+type Story = StoryObj<typeof Alert>;
+
+export const DefaultAlert: Story = {
+  args: {
+    variant: 'default',
+    children: (
+      <>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </>
+
+    )
+  }
+};
