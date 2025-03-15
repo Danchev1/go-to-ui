c5f9245c553c4a23de9c330ec3b809a21b3d8f85
yordandanchev <yordangdanchev@gmail.com>
2025-03-15 17:21:04 +0200

    - Add Alert Story for Alert Component

--- lib/Alert/Alert.stories.tsx
+++ lib/Alert/Alert.stories.tsx
@@ -27,11 +27,8 @@ export const DefaultAlert: Story = {
       <>
         <Terminal className="h-4 w-4" />
         <AlertTitle>Heads up!</AlertTitle>
-        <AlertDescription>
-          You can add components to your app using the cli.
-        </AlertDescription>
+        <AlertDescription>You can add components to your app using the cli.</AlertDescription>
       </>
-
     )
   }
 };
